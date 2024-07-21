14da991072c507c39f074869b2dcda91c987fc96
Farha Islam <farha.islam@students.codeinstitute.net>
2024-07-21 12:40:18 +0000

    Update Truested Origins

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=@&os&ch^33fh9a$+mo2q#fa6p6enadiwt)$*v%sg-hk3h)i('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [    "8000-farhatamann-djangoblog2-eyktloyhtbr.ws.codeinstitute-ide.net",
+ALLOWED_HOSTS = ["8000-farhatamann-djangoblog2-eyktloyhtbr.ws.codeinstitute-ide.net",
     ".herokuapp.com"]
 
 
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
     # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_urlfarha.islam@students.codeinstitute.net.parse(os.environ.get("DATABASE_URL"))
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeanyapp.com",
+    "https://*.codeinstitute-ide.net",
     "https://*.herokuapp.com"
 ]
 
